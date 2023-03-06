565e5a35c5b7edb9effca12c16180379a3cde757
Carolina Pedrosa <carolinasouza0@gmail.com>
2023-03-06 18:28:17 -0300

    adicionando requisito 9

--- src/challenges.js
+++ src/challenges.js
@@ -103,6 +103,24 @@ const fizzBuzz = (array) => {
 
 // Desafio 9 - Crie a função encode e a função decode
 
+const encode = (string) => {
+  return string
+    .replace(/a/g, 1)
+    .replace(/e/g, 2)
+    .replace(/i/g, 3)
+    .replace(/o/g, 4)
+    .replace(/u/g, 5);
+};
+
+const decode = (string) => {
+  return string
+    .replace(/1/g, 'a')
+    .replace(/2/g, 'e')
+    .replace(/3/g, 'i')
+    .replace(/4/g, 'o')
+    .replace(/5/g, 'u');
+};
+
 // Desafio 10 - Crie a função techList
 
 // Não modifique essas linhas
