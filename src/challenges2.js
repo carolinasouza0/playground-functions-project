ec5fbe9f7aede6ef392fa44c02b8a9b48012d5b5
Carolina Pedrosa <carolinasouza0@gmail.com>
2023-03-07 14:02:52 -0300

    adicionando requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,5 +1,32 @@
 // Desafio 11 - Crie a função generatePhoneNumber
 
+const countIquals = (array) => {
+  let counts = {};
+  array.forEach((count) => {
+    counts[count] = (counts[count] || 0) + 1;
+  });
+  const iquals = Math.max(...Object.values(counts));
+  return iquals;
+};
+
+const errorMessage = (array) => {
+  let error = '';
+
+  if (array.length > 11 || array.length < 11) {
+    return (error = 'Array com tamanho incorreto.');
+  }
+  array.forEach((elem) => {
+    if (elem < 0 || elem > 9 || countIquals(array) >= 3) {
+      error = 'não é possível gerar um número de telefone com esses valores';
+    }
+  });
+  return error;
+};
+
+const generatePhoneNumber = (array) => {
+  let number = `(${array[0]}${array[1]}) ${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}-${array[7]}${array[8]}${array[9]}${array[10]}`;
+  return errorMessage(array) || number;
+};
 // Desafio 12 -  Crie a função triangleCheck
 
 // Desafio 13 - Crie a função hydrate
@@ -8,7 +35,8 @@
 
 // Não modifique essas linhas
 module.exports = {
-  generatePhoneNumber: typeof generatePhoneNumber === 'function' ? generatePhoneNumber : (() => {}),
-  triangleCheck: typeof triangleCheck === 'function' ? triangleCheck : (() => {}),
-  hydrate: typeof hydrate === 'function' ? hydrate : (() => {}),
+  generatePhoneNumber:
+    typeof generatePhoneNumber === 'function' ? generatePhoneNumber : () => {},
+  triangleCheck: typeof triangleCheck === 'function' ? triangleCheck : () => {},
+  hydrate: typeof hydrate === 'function' ? hydrate : () => {},
 };
